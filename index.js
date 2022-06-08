6000374db46b5ea3a13fa849519b8b4a7a22372b
pau <paugitonga6@gmail.com>
2022-06-08 14:40:01 +0300

    commit tree dab48d4752ec9ef37afd4827524d1915b95a58b3

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let multiply;
+let num1=2;
+let num2=31;
+multiply=num1*num2;
+let random=Math.floor(Math.random()*1000+1)
+let num3=49, num4=5;
+let mod= num3%num4;
+let max=Math.max(1,2,3,4,5,6,7,8,9,12,13,17,20);
